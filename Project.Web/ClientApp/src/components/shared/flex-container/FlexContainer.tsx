52aa121a31339945c1b2e428c81db35824f5bc5c
PegasUsOnce <rosliakovaov@gmail.com>
2022-03-20 14:25:30 +1000

    flex container с gap

---
+++ Project.Web/ClientApp/src/components/shared/flex-container/FlexContainer.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './FlexContainer.module.scss'
+
+export type FlexDirection = 'vertical' | 'horizontal'
+
+interface IProps {
+  gap: string,
+  width?: string,
+  margin?: string,
+  direction: FlexDirection,
+  children?: React.ReactNode
+}
+
+const FlexContainerImpl: React.FC<IProps> = ({ gap, direction, width = undefined, margin = undefined, children }) => {
+  const className = styles.flexContainer + ' ' + (direction === 'vertical' ? styles.vertical : styles.horizontal)
+  const style = { gap: gap, margin: margin, width: width }
+
+  return <div className={className} style={style} >
+    {children}
+  </div>
+}
+
+export const FlexContainer = React.memo(FlexContainerImpl)
